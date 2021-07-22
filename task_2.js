59797fe8668f879e9c96bcef22d18f2e1a1f2c21
AndreiGR <andryigr@gmail.com>
2021-07-22 18:29:52 +0300

    lesson-1_2 Delite margins

--- task_2.js
+++ task_2.js
@@ -1,41 +1,34 @@
+
 //аргументом передаем старртовое значение
 
-let numHandler = function(num) {
+let numHandler = function (num) {
 
   let startValue = num;
-
   let sumValue = 2;
 
-  return function() {
+  return function () {
 
     if (startValue % 5 === 0 & !(startValue === 0)) {
 
       sumValue = 3;
-
       startValue /= 5;
 
       return startValue;
-
     }
 
-    if (startValue % 7 === 0 & !(startValue === 0)) {
+      if (startValue % 7 === 0 & !(startValue === 0)) {
 
-      sumValue = 1;
+        sumValue = 1;
+        startValue -= 7;
 
-      startValue -= 7;
-
-      return startValue;
-
-    }
-
-    return startValue += sumValue;
-
+        return startValue;
+      }
+      return startValue += sumValue;
   }
-
 }
 
-for (let i = 1; i < 20; i++) {
-  let someValue = Math.floor(Math.random() * 10) + 1;
+for (let i = 1 ;i < 20 ; i++){
+  let someValue = Math.floor(Math.random()*10)+1;
   console.log(`
     Вызванно с аргументом ${someValue}
     возращаемое значение ${numHandler(someValue)()}`);
